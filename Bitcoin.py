01afb17d162d019e8ff37b4f3883a0853bf06ca4
Shijie Wang <shijiehkj@gmail.com>
2018-03-08 00:57:14 -0500

    finishes piecewise fitting and double differencing;supports quadratic trend and GARCH

--- Bitcoin.py
+++ Bitcoin.py
@@ -35,12 +35,6 @@ def get_data(date):
 series = get_data(today)
 
 
-# In[3]:
-
-series.sort()
-series
-
-
 # In[8]:
 
 
@@ -104,17 +98,17 @@ daily_data()
 
 
 
-# In[2]:
+# In[19]:
 
-dt = pd.read_csv("C:\\Users\\Shijie Wang\\Desktop\\Data Scientist\\GitWeb\\DianBlog\\Data till 20180218.csv")
-dt = dt[:-2]
+series
+dt = pd.DataFrame(series,columns = ['Date','Close Price'])
 dt['Date'] =  pd.to_datetime(dt['Date'])
-dt.dtypes
+dt
 
 
 # #  We could use the basic plot function, but it doesn't look so pretty. We want to examine piecewise trend due to the volatility of price trend
 
-# In[3]:
+# In[7]:
 
 
 plt.plot(dt['Date'],dt['Close Price'])
@@ -122,7 +116,7 @@ plt.show()
 dt
 
 
-# In[4]:
+# In[8]:
 
 # test
 import plotly.plotly as py
@@ -198,7 +192,7 @@ py.iplot(fig, filename = "Bitcoin Price with Slider for Inspection")
 # utils.install_packages('MASS')
 # ```
 
-# In[5]:
+# In[76]:
 
 #import rpy2.tests
 #import unittest
@@ -221,29 +215,16 @@ pi = robjects.r['pi']
 pi[0]
 
 
-# In[36]:
-
-
-
-r = robjects.r
-dt['tt'] = list(range(1,len(dt)+1))
-dt
-
-from rpy2.robjects import r,pandas2ri
-pandas2ri.activate()
-
-r_dt = pandas2ri.py2ri(dt)
-r_dt.rx(2)
-
-
-# In[81]:
+# In[28]:
 
 from rpy2.robjects import r,pandas2ri
 pandas2ri.activate()
 
 r = robjects.r
+
+#dt = dt[dt['Date'] > '2017-01-01']
+
 dt['tt'] = list(range(1,len(dt)+1))
-dt = dt[dt['Date'] > '2017-01-01']
 
 
 fmla = robjects.Formula('Close.Price ~ tt')
@@ -253,53 +234,98 @@ env['tt'] = dt['tt']
 stats = importr('stats')
 
 lm_basic = stats.lm(fmla)
-#brk = struc.breakpoints()
+
 print(base.summary(lm_basic))
 type(lm_basic)
 
 
-# In[82]:
+# In[166]:
 
-seg = rpackages.importr('segmented')
+d1 = dt.loc[dt['Date'] == '2017-09-14']['tt']
+d2 = dt.loc[dt['Date'] == '2017-12-16']['tt']
+d3 = dt.loc[dt['Date'] == '2018-02-05']['tt']
+
+
+# In[96]:
+
+seg = importr('segmented')
 seg_m = seg.segmented(lm_basic)
 #print(base.summary(seg_m))
 
 print(seg_m.names)
 print(seg.slope(seg_m))
-
-#using all historical data makes it harder to detect breakpoints
-
-
-# In[89]:
-
-dt = pd.read_csv("C:\\Users\\Shijie Wang\\Desktop\\Data Scientist\\GitWeb\\DianBlog\\Data past year.csv")
-dt = dt[:-2]
-dt['Date'] =  pd.to_datetime(dt['Date'])
-dt.dtypes
-dt['tt'] = list(range(1,len(dt)+1))
+print(seg_m.rx2("psi"))
+dt.iloc[2339,0]
+#If not specifcing psi starting point, giving only 1 breakpoint
 
 
-fmla = robjects.Formula('Close.Price ~ tt')
-env = fmla.environment
-env['Close.Price'] = dt['Close Price']
-env['tt'] = dt['tt']
-stats = importr('stats')
+# ## When using segmented function, an estimate of the starting point for breakpoints needs to be supplied. Increasing breakpoints will increase model parameters. Hence, BIC could be used for the best breakpoints selection. 
+# 
+# In this example, we can easily judge the breakpoints by visual inspection. For general prediction, below algorithm could be implemented: 
+# 1. Apply k folds validation on the time series. Note that it's slightly different from the pooled data because it's continuous and whatever happened in year 1 and 2 could possibly affect years after. So the training will be done like this: 
+#     1. Train year 1 -> Test year 2
+#     2. Train year 1 and 2 -> Test year 3 
+#     ...
+#     7. Train year 1 to 7 -> Test year 8
+# 2. Feed all possible combinations of breakpoints and get corresponding model's average BIC for each of the K fold traning  
+# 3. Choose the model given the highest BIC
 
-lm_basic = stats.lm(fmla)
+# In[167]:
+
 #brk = struc.breakpoints()
 print(base.summary(lm_basic))
 type(lm_basic)
-psi_v = robjects.IntVector([250,360])
+psi_v = robjects.IntVector([d1,d2,d3])
 seg_m = seg.segmented(lm_basic, psi = psi_v)
 #print(base.summary(seg_m))
 
 print(seg_m.names)
 print(seg.slope(seg_m))
-
-
-# In[92]:
-
 print(seg_m.rx2("psi"))
+print(dt.iloc[2421],dt.iloc[2563],dt.iloc[2592])
+
+
+# In[168]:
+
+print(seg.slope(seg_m)[0],seg_m.rx2("psi") )
+
+
+# In[169]:
+
+breaks = [dt.iloc[i,0] if i == 0 else dt.iloc[int(seg_m.rx2("psi").rx(i,2)[0]),0] for i in range(4)]
+slopes = [seg.slope(seg_m)[0].rx(i+1,1)[0] for i in range(4)]
+
+print(breaks)
+print(slopes)
+
+
+# In[176]:
+
+res = [seg_m.rx2("residuals")[i] for i in range(len(seg_m.rx2("residuals")))]
+plt.plot(res)
+plt.show()
+
+secnd_diff = pd.Series(res).diff()
+plt.plot(secnd_diff)
+plt.show()
+
+
+# In[180]:
+
+pd_17 = dt[dt['Date'] >='2017-01-01']
+first_dif_17 = pd_17['Close Price'].diff()
+second_dif_17 = pd_17['Close Price'].diff().diff()
+plt.plot(first_dif_17)
+plt.show()
+plt.plot(second_dif_17)
+plt.show()
+
+#OK.. Use GARCH model. 
+
+
+# In[156]:
+
+len(seg_m.rx2("residuals"))
 
 
 # In[ ]:
